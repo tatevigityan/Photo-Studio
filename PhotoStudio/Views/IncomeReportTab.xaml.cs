442fcc370299fed1bfad18801c949ba4c6ffb845
tatevigityan <tatev.igityan@mail.ru>
2023-12-09 12:09:59 +0300

    Revert "Revert "Created main Views""
    This reverts commit 1f4edb17531aebfdfd550025f2c4e3739e001447.

---
+++ PhotoStudio/Views/IncomeReportTab.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using PhotoStudio.ViewModels;
+
+namespace PhotoStudio
+{
+    public partial class IncomeReportTab : UserControl
+    {
+        public IncomeReportTab(ApplicationViewModel appViewModel)
+        {
+            InitializeComponent();
+            DataContext = appViewModel;
+        }
+    }
+}
---
+++ PhotoStudio/Views/StatisticsTab.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using PhotoStudio.ViewModels;
+
+namespace PhotoStudio
+{
+    public partial class StatisticsTab : UserControl
+    {
+        public StatisticsTab(ApplicationViewModel appViewModel)
+        {
+            InitializeComponent();
+            DataContext = appViewModel;
+        }
+    }
+}
