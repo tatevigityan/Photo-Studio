ebfa5b93bba3e0dcfe0cac0c4d70a839638c5843
tatevigityan <tatev.igityan@mail.ru>
2023-12-23 22:42:19 +0300

    Small fixes

---
+++ PhotoStudio/Views/Tabs/TabBooking.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using PhotoStudio.ViewModels;
+
+namespace PhotoStudio
+{
+    public partial class TabBooking : UserControl
+    {
+        public TabBooking(ApplicationViewModel appViewModel)
+        {
+            InitializeComponent();
+            DataContext = appViewModel;
+        }
+    }
+}
